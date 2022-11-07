cfe1fd2a9f84fe044f0b7ac3a16a6f9393412363
Mohammed@almallahi98 <x52-@hotmail.com>
2022-11-07 10:17:48 +0300

    last update

--- src/component/Home/HomeComp/Hero.jsx
+++ src/component/Home/HomeComp/Hero.jsx
@@ -4,29 +4,34 @@ import {
     Flex,
     Text,
     VStack,
+    Center,
     useBreakpointValue,
     Box,
+    SimpleGrid,
 } from "@chakra-ui/react";
 
 function Hero() {
     return (
         <Box>
-            
-            
+
+
             <Flex
                 w={"full"}
                 h={"100vh"}
+                direction={'column'}
+                bg={'black'}
                 backgroundImage={window.location.origin + "/resources/imges/iot.jpeg"}
                 backgroundSize={"cover"}
                 backgroundPosition={"center center"}
-                
+
             >
                 <VStack
                     w={"full"}
+                    h={'100%'}
                     justify={"center"}
                     textAlign={'center'}
-                    px={useBreakpointValue([0,2,0,0])}
-                    bgGradient={"linear(to-t, rgba(12,26,44,.9) 25%, transparent)"}
+                    px={useBreakpointValue([0, 2, 0, 0])}
+                    bgGradient={"linear(to-t, rgba(12,26,44,1) 25%, transparent)"}
                 >
                     <Stack maxW={"90%"} align={"flex-end"} spacing={4} >
                         <Text
@@ -42,18 +47,37 @@ function Hero() {
                             in various industries.
                         </Text>
                     </Stack>
-                    <Box w={'100%'} minH={'20vh'}>
-                    
-                        <Box mt={'20vh'}  w={'100%'} minH={'45vh'} bg={'white'}>
-                            {/* the pics gose hear !!  ['90%','300px','350px','350px']*/}
+                    {/* <Box w={'100%'} minH={'20vh'}>
 
-                        </Box>
+                        <Box mt={'20vh'} w={'100%'} minH={'45vh'} bg={'rgba(12,26,44,1)'}>
+                            the pics gose hear !!  ['90%','300px','350px','350px']
 
-                    </Box>
+                        </Box> 
+
+                    </Box> */}
+
                 </VStack>
-                
+
             </Flex>
-            
+            <Center w={'100%'} minH={'50vh'} bg={'rgba(12,26,44,1)'}>
+                {/* the pics gose hear !!  ['90%','300px','350px','350px']*/}
+                <Box w={'100%'} h={'90%'} bg={'white'}>
+                    <Center>
+                        <Text fontSize={"2xl"} textColor={'black'} pt={'3px'}>
+                            {/*entrest*/}
+                            hi
+                        </Text>
+                        <SimpleGrid columns={[2, null, 3]} spacing='40px'>
+                                <Box bg='tomato' height='100%'></Box>
+                                <Box bg='tomato' height='100%'></Box>
+                                <Box bg='tomato' height='100%'></Box>
+                                <Box bg='tomato' height='100%'></Box>
+                                <Box bg='tomato' height='100%'></Box>
+                            </SimpleGrid>
+                    </Center>
+
+                </Box>
+            </Center>
         </Box>
     );
 }
