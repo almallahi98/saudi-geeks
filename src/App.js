dfd3a1ed9e6fbbc1751cf8e9d31e90d61ece8ecd
Mohammed@almallahi98 <x52-@hotmail.com>
2022-11-07 16:25:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 //import logo from './logo.svg'; our icon gose here
 import { Route, Routes } from 'react-router';
 import './App.css';
-import Nav from './component/NavBar/Nav';
-import About from './component/About_us/About_us'
+// import Nav from './component/NavBar/Nav';
+// import About from './component/About_us/About_us'
 
 //paths
 import Home from './component/Home/Home'
