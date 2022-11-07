8411ec9753ff9b6c4c40927b2aca610eef5bbf11
Mohammed@almallahi98 <x52-@hotmail.com>
2022-11-07 15:21:28 +0300

    save

--- src/component/Home/HomeComp/meth/MethodOfWork.jsx
+++ src/component/Home/HomeComp/meth/MethodOfWork.jsx
@@ -7,18 +7,21 @@ function MethodOfWork() {
             <Box bg={'yellow'} p={'15px'}>
                 <Flex my={'5px'}>
                     <Box w={'100%'} bg={'green'} p={'15px'}>
-                        <Heading>Method of work</Heading>
-                        <Text fontSize={'2xl'}>fghkjlklvgbhnj, bnm</Text>
-                        <Heading fontSize={'2xl'}>FrontEnd</Heading>
+                        <Heading mb={'10px'}>Project abstraction</Heading>
+                        <Text mb={'10px'}>fghkjlklvgbhnj, bnm</Text>
+                        <Heading fontSize={'xl'}>FrontEnd</Heading>
                         <Text>since there's no visible and possible way to show row data to the user
                             it became responsible to use a front-end framework such as React Js,
                             and in our case, we will use it, its the best choice that will meet our
                             requirements.</Text>
-                        <Heading fontSize={'2xl'}>BackEnd(EndPoint)</Heading>
+                        <Heading fontSize={'xl'}>BackEnd(EndPoint)</Heading>
                         <Text>fvgbhnjmk,l</Text>
-                        <Heading fontSize={'2xl'}>Device Connectivity</Heading>
+                        <Heading fontSize={'xl'}>Node's (Devices)</Heading>
+                        <Text>fvgbhnjmk,l</Text>
+                        <Heading fontSize={'3xl'}>Method of work</Heading>
                         <Text>fvgbhnjmk,l</Text>
                     </Box>
+                    
                     <Box w={'100%'} bg={'blue'}>
                         <p>2</p>
                     </Box>
