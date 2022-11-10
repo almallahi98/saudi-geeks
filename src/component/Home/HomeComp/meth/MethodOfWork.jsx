dea0227fc6fa9e4ed1d538759b567afdcd45b1ee
Mohammed@almallahi98 <x52-@hotmail.com>
2022-11-10 12:47:46 +0300

    last 12:47pm

--- src/component/Home/HomeComp/meth/MethodOfWork.jsx
+++ src/component/Home/HomeComp/meth/MethodOfWork.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Heading } from '@chakra-ui/react'
+import { Box, Flex, Text, Heading, Image } from '@chakra-ui/react'
 import React from 'react'
 import Drop from './Drop'
 
@@ -6,9 +6,14 @@ function MethodOfWork() {
     return (
         <>
             <Box p={'15px'}>
-                <Box bg={'whiteAlpha.400'} h={'35vh'} w={'100%'} p={'15px'}>
+                <Box bg={'whiteAlpha.400'} h={'100%'} w={'100%'} p={'15px'}>
                     <Heading mb={'10px'}>Project abstraction</Heading>
-                    <Text mb={'10px'}>fghkjlklvgbhnj, bnm</Text>
+                    <Text mb={'10px'}>the main functionality of this project, is to simulate the human daily activity.
+                    taking in mind the huge advantages and the impact the the industry. </Text>
+                    <Flex direction={'row'}>
+                    <Image w={'50%'} h={'90vh'} mp={'50vh'} p={'4px'} alt='' src='/resources/imges/iota-iot_system.png'/>
+                    <Image w={'50%'} h={'90vh'} mt={'50vh'} p={'4px'} alt='' src='/resources/imges/Aeris_Industries-2-scaled-1.jpg'/>
+                    </Flex>
                 </Box>
                 <Flex mt={'5px'}>
                     <Box w={'100%'} bg={'whiteAlpha.400'}  p={'15px'} mr={'2px'}>
